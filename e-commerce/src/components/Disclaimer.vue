ff087e8e4e167cb522d684e655e522b2fff2701e
Nathan-reitan <nathanreitan@gmail.com>
2021-12-15 16:54:32 -0800

    state not working, need to change to instantiated components I think.

--- e-commerce/src/components/Disclaimer.vue
+++ e-commerce/src/components/Disclaimer.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        <h1 class="disclaimer">
-            This site is for demonstrative purposes only, nothing is actually for sale.  DO NOT ENTER ANY CREDIT CARD INFORMATION INTO THIS SITE.
+    <div class="disclaimer">
+        <h1>
+            This site is for demonstrative purposes only, nothing is actually for sale. 
         </h1>
+        <h1>
+             DO NOT ENTER ANY CREDIT CARD INFORMATION INTO THIS SITE.
+        </h1>
+        <h2>
+            Scroll down to see site.
+        </h2>
     </div>
 </template>
 
--- e-commerce/src/components/Products.vue
+++ e-commerce/src/components/Products.vue
@@ -1,20 +1,20 @@
 <template>
     <main>
-        <div class="peletonRow">
+        <div class="productRow">
             <h1>Peleton</h1>
             <div class="w-100 px-2">
                 <img src="../assets/peloton.png" alt="Peloton Bike" class="w-100">
                 <p>Peloton is the top of the line for stationary bikes.  They've integrated online courses and ranking systems to keep work outs from home engaging and driven.</p>
             </div>
         </div>
-        <div class="peletonRow">
+        <div class="productRow">
             <h1>Mirror</h1>
             <div class="w-100 px-2">
                 <img src="../assets/mirror.png" alt="Peleton Bike" class="w-100">
                 <p>This isn't just a mirror. It's a cardio class, it's a yoga studio, it's a boxing ring, it's your new personal trainer, and it's so much more.</p>
             </div>
         </div>
-        <div class="peletonRow">
+        <div class="productRow">
             <h1>Hydrow Rower</h1>
             <div class="w-100 px-2">
                 <img src="../assets/hydrowVert.png" alt="Peleton Bike" class="w-100">
